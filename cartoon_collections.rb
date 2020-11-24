f7d73d361c33153c58a4dd7f7ee340e20d8e6b3d
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-24 15:50:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   list_array = array
   array.each_with_index do |item, index|
-    index > 1
+    index += 1
     puts "#{index} #{item}"
 end
 end
